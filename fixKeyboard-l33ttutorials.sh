17bbdf83038902128506d57b5feeab2571506d21
xODINxbr <rafael.cucharo86@gmail.com>
2018-05-26 11:59:39 -0300

    Create fixKeyboard-l33ttutorials.sh

---
+++ fixKeyboard-l33ttutorials.sh
@@ -0,0 +1,6 @@
+#!/bin/bash/
+echo "Rewriting   /etc/default/keyboard   file"
+
+echo '# KEYBOARD CONFIGURATION FILE''\n\n''# Consult the keyboard(5) manual page''\n\n''XKBMODEL="pc105"''\n''XKBLAYOUT="us"''\n''XKBVARIANT="altgr-intl"''\n''XKBOPTIONS=""''\n\n''BACKSPACE="guess"' > /etc/default/keyboard
+
+sudo reboot
